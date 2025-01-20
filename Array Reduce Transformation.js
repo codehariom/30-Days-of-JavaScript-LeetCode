e2580c9a8fa70d851c01fa90bb62d2119e6570fc
Hariom Gupta <digitalwithhariom@gmail.com>
2025-01-20 07:52:02 +0530

    Create Array Reduce Transformation.js

---
+++ Array Reduce Transformation.js
@@ -0,0 +1,30 @@
+
+// Example 1:
+
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr; }
+// init = 0
+// Output: 10
+// Explanation:
+// initially, the value is init=0.
+// (0) + nums[0] = 1
+// (1) + nums[1] = 3
+// (3) + nums[2] = 6
+// (6) + nums[3] = 10
+// The final answer is 10.
+
+// CODE 
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let total = init
+    for(let i = 0; i< nums.length; i++){
+        total = fn( total, nums[i])
+    }
+    return total
+};
